64c312bb44403a5c95b4497b11c81ad06dd81633
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-07-25 20:02:50 +0100

    Done without database

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react"
+import AddTask from "./components/AddTask";
 
-function App() {
+
+import React from 'react'
+
+const App = () => {
+  const [tasks, setTasks] = useState(
+    [
+        // {
+        //     id: 1,
+        //     text: "Doctors Appointment",
+        //     day: 'Feb 5th at 2:30pm',
+        //     reminder: true,
+        // },
+        // {
+        //     id: 2,
+        //     text: "Meeting at school",
+        //     day: 'Feb 6th at 1:30pm',
+        //     reminder: true,
+        // },
+        // {
+        //     id: 3,
+        //     text: "Food shopping",
+        //     day: 'Feb 5th at 2:30pm',
+        //     reminder: false,
+        // }
+    ]
+)
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggel reminder
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+  }
+
+  //Add Task
+
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) +1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  const [showAddTask, setShowAddTask] = useState(true)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask) } showAdd={showAddTask} title='Task Tracker'/>
+      {showAddTask && <AddTask onAdd={addTask} />}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onToggle={toggleReminder} onDelete={deleteTask} />) : (
+        'There is no task to show'
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
